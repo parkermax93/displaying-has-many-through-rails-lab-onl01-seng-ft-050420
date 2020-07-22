e31a637af9d72156ff628a7f645d80ec09470923
Parker Williamson <parker.williamson@gmail.com>
2020-07-22 20:53:58 +0000

    Automatically backed up by Learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,12 @@
 class Appointment < ApplicationRecord
-  belongs_to :patient
-  belongs_to :doctor
+    belongs_to :patient
+    belongs_to :doctor
+
+    def doctor_name
+        self.doctor.name
+    end
+
+    def patient_name
+        self.patient.name
+    end
 end
