46c5741e0489a5537fb2cc36047057b060842006
Parker Williamson <parker.williamson@gmail.com>
2020-07-22 20:24:41 +0000

    Automatically backed up by Learn

--- db/migrate/20200722202427_create_doctors.rb
+++ db/migrate/20200722202427_create_doctors.rb
@@ -1,7 +1,7 @@
 class CreateDoctors < ActiveRecord::Migration[5.0]
   def change
     create_table :doctors do |t|
-      t.string, :name
+      t.string :name
       t.string :department
 
       t.timestamps
