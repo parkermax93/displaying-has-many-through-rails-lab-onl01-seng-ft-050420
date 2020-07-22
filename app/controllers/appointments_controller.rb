144c18d5397f75200d4baff69393e2ba77fd8f23
Parker Williamson <parker.williamson@gmail.com>
2020-07-22 20:55:41 +0000

    Automatically backed up by Learn

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,5 +1,10 @@
 class AppointmentsController < ApplicationController
-    def show 
-        @appointment = Appointment.find(params[:id])
+    def show
+        @appointment = Appointment.find_by_id(params[:id])
     end
+
+    private
+        def appointment_params
+            params.require(:appointment).permit(:appointment_datetime)
+        end
 end
